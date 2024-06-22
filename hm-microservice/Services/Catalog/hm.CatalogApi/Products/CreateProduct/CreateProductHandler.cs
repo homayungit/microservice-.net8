3a13736b31b5de5508872ef8373fc28909e95e04
homayungit <homayunjust@gmail.com>
2024-06-23 02:29:09 +0600

    Commit All Code for Section No-5

---
+++ hm-microservice/Services/Catalog/hm.CatalogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using hm.CatalogApi.Models;
+
+namespace hm.CatalogApi.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Create product entity from command object
+        //Save to database
+        //return createproductresult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+        //Save to database
+
+        return new CreateProductResult(Guid.NewGuid());        
+    }
+}
+
---
+++ hm-microservice/src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Data;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+
+}
+
